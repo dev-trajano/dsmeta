90ab2ead3d61a984a214b450cae71eb30f7c94a5
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-07-15 19:48:47 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from './componets/Header';
 import NotificationButton from './componets/NotificationButton';
+import SalesCard from './componets/SalesCard';
 
 function App() {
   return (
     <>
-      <h1>guerra</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
--- frontend/src/componets/NotificationButton/index.tsx
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -1,11 +1,11 @@
-import image from '../../assets/img/notification-icon.svg';
+import notification from '../../assets/img/notification-icon.svg';
 import './styles.css';
 
 function NotificationButton() {
   return (
     <>
       <div className="dsmeta-red-btn">
-        <img src={image} alt="Notificar" />
+        <img src={notification} alt="Notificar" />
       </div>
     </>
   );
